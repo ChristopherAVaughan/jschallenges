2cb5e526bfb51159500588321588a86804e7665b
Christopher <crisv76@icloud.com>
2016-09-15 09:19:48 -0400

    all but audio done

--- challenge_2_alt/challenge2_alt.js
+++ challenge_2_alt/challenge2_alt.js
@@ -24,12 +24,75 @@
  * OPTIONAL STUFF:
  * - add logos of the two rivals below their name
  * - make the page just look better
- * - play a sound when someone clicks the "Shoot" button. You'll need to read about the <audio> element
+ * PUNT - play a sound when someone clicks the "Shoot" button. You'll need to read about the <audio> element
  *   and how to use it in JS. You will also need to download a sound bite
  */
 
+ 
  $(function(){
+ 
+ 	// MODEL
+    // Source of truth
+    var capsShoot = $('#caps-shoot');
+    var pensShoot = $('#pens-shoot');
+    var reset = $('#reset');
+    var capsShotCount = 0;
+    var capsGoalCount = 0;
+    var pensShotCount = 0;
+    var pensGoalCount = 0;
+    var resetCount = 0;
 
+	//VIEW
+	//Rendering data to user
+	document.body.style.backgroundImage = "url('images/ice.gif')";
 
+  function render () {
+	    $('#caps-numshots').text(capsShotCount);
+	    $('#caps-numgoals').text(capsGoalCount);
+	    $('#pens-numshots').text(pensShotCount);
+	    $('#pens-numgoals').text(pensGoalCount);
+	    $("#num-resets").text(resetCount);
+	}
 
- })
+	//CONTROLLER
+	//Caps shots and goals
+	$('#caps-shoot').click(function() {
+          capsShotCount++;
+          var random = Math.random();
+        	if (random <= 0.27) {
+        		capsGoalCount++;
+        		document.body.style.backgroundImage = "url('images/RockinRed.gif')";
+          }
+          render();
+      })
+	
+	//Pens shots and goals
+    $('#pens-shoot').click(function() {
+          pensShotCount++;
+          var random = Math.random();
+        	if (random <= 0.18) {
+        		pensGoalCount++;
+        		document.body.style.backgroundImage = "url('images/PenguinPYellow.gif')";
+        	}	
+          render();
+      })
+
+	//Reset counter
+    $('#reset').click(function() {
+       	capsShotCount = 0;
+    	capsGoalCount = 0;
+    	pensShotCount = 0;
+     	pensGoalCount = 0;
+     	resetCount++;
+     	$('#caps-numshots').text(capsShotCount);
+	    $('#caps-numgoals').text(capsGoalCount);
+	    $('#pens-numshots').text(pensShotCount);
+	    $('#pens-numgoals').text(pensGoalCount);
+	    $("#num-resets").text(resetCount);
+      document.body.style.backgroundImage = "url('images/ice.gif')";
+	    render();
+	})
+	
+
+    
+ }) //end of total function
